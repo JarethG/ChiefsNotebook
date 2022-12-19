0b47a5c4885c4c89d4ae83e2d08164ed1245f590
unknown <jarethgaskin@gmail.com>
2022-12-19 14:48:57 +1300

    meals have general layout drawn in to the meal plan tab

--- App.js
+++ App.js
@@ -1,50 +1,66 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import {ScrollView, Text, View} from 'react-native';
 import {Styles} from "./Styles";
 import {Ionicons} from "@expo/vector-icons";
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 function History() {
-  return (
-      <View style={Styles.background}>
-        <Text>History!</Text>
-      </View>
-  );
+    return (
+        <View style={Styles.background}>
+            <Text>History!</Text>
+        </View>
+    );
 }
 
 function MealPlan() {
-  return (
-      <View style={Styles.background}>
-        <Text>Home!</Text>
-      </View>
-  );
+    return (
+        <View style={Styles.background}>
+            <ScrollView style={{width:"100%"}} contentContainerStyle={{alignItems:"center"}}>
+                {["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].map((day, index) =>
+                    <View key={index} style={{width:"90%"}}>
+                        <Text>{day}</Text>
+                        <View style={Styles.mealCard}>
+                            <View><Text>image</Text></View>
+                            <Text>meal name</Text>
+                            <View style={{flexDirection:"row",width:"100%",justifyContent:'space-around'}}>
+                                <Text>time</Text>
+                                <Text>skill level</Text>
+                                <Text>serving size</Text>
+                            </View>
+                        </View>
+                    </View>
+                )}
+            </ScrollView>
+            <Text>Home!</Text>
+        </View>
+    );
 }
 
 function SettingsScreen() {
-  return (
-      <View style={Styles.background}>
-        <Text>Settings!</Text>
-      </View>
-  );
+    return (
+        <View style={Styles.background}>
+            <Text>Settings!</Text>
+        </View>
+    );
 }
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="History" component={History} options={{
-              tabBarIcon: ({color}) => <Ionicons name="bar-chart-outline" size={24} color="black" />
-          }}/>
-          <Tab.Screen name="Meal Plan" component={MealPlan} options={{
-              tabBarIcon: ({color}) => <Ionicons name="calendar-outline" size={24} color="black" />
-          }}/>
-          <Tab.Screen name="Settings" component={SettingsScreen} options={{
-              tabBarIcon: ({color}) => <Ionicons name="settings-outline" size={24} color="black" />
-          }}/>
-        </Tab.Navigator>
-      </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Meal Plan" component={MealPlan} options={{
+                    tabBarIcon: ({color}) => <Ionicons name="calendar-outline" size={24} color="black"/>
+                }}/>
+                <Tab.Screen name="History" component={History} options={{
+                    tabBarIcon: ({color}) => <Ionicons name="bar-chart-outline" size={24} color="black"/>
+                }}/>
+                <Tab.Screen name="Settings" component={SettingsScreen} options={{
+                    tabBarIcon: ({color}) => <Ionicons name="settings-outline" size={24} color="black"/>
+                }}/>
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
 }
--- Styles.js
+++ Styles.js
@@ -5,5 +5,19 @@ export const Styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
-    }
+    },
+    mealCard: {
+        margin: 10,
+        backgroundColor: "white",
+        borderRadius: 20,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
 });
