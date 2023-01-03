2a060c4186a0a4dd8708e1ca76c100190fd1c9c8
unknown <jarethgaskin@gmail.com>
2023-01-03 15:43:06 +1300

    recipe UX improved.

--- Recipes/RecipeDisplay.js
+++ Recipes/RecipeDisplay.js
@@ -3,21 +3,20 @@ import {Styles} from "../Styles";
 import * as React from "react";
 import {localImage} from "../assets/localImageLoader";
 import {useState} from "react";
+import {Ionicons} from "@expo/vector-icons";
 
-export const RecipeDisplay = ({recipe,day}) => {
+export const RecipeDisplay = ({recipe, day}) => {
+
+    const image = recipe.imageURL ? {uri: recipe.imageURL} : localImage(recipe.name);
+
     const [modalVisible, setModalVisible] = useState(false);
     return <>
-        <View style={Styles.mealCard}>
+        <View style={Styles.meal_card}>
             <Text style={Styles.header}>{day}</Text>
             <Pressable onPress={() => {
                 setModalVisible(true)
             }}>
-                {recipe.imageURL ?
-                    <Image source={{uri: recipe.imageURL}} style={{width: 200, height: 200}}/>
-                    :
-                    <Image source={localImage(recipe.name)} style={{width: 200, height: 200}}/>
-
-                }
+                <Image source={image} style={Styles.meal_card_image}/>
                 <Text>
                     {recipe.name}
                 </Text>
@@ -27,24 +26,31 @@ export const RecipeDisplay = ({recipe,day}) => {
         <Modal
             animationType="slide"
             transparent={true}
-            visible={modalVisible}>
-            <ScrollView style={{backgroundColor: "white", flex: 1}}>
-                <Text style={Styles.header}> {recipe.name}</Text>
-                {recipe.ingredients.map((e, i) => {
-                    return <Text key={i}>
-                        {e.quantity} {e.name}
-                        {/*{e.type}*/}
-                    </Text>
-                })}
-                {recipe.steps.map((e, i) => {
-                    return <Text key={i} style={Styles.recipe_steps}>
-                        {e}
-                    </Text>
-                })}
-            </ScrollView>
-            <View>
-                <Button title={"back"} onPress={() => setModalVisible(false)}/>
+            visible={modalVisible}
+        onRequestClose={()=>setModalVisible(false)}>
+            <View style={{backgroundColor: "white", flex: 1}}>
+                <Image source={image} style={{postition: "absolute", width: "100%", height: "30%"}}/>
+                <Pressable style={{position: "absolute",top:0,flexDirection:"row",alignItems:"center",backgroundColor:"rgba(160,160,160,0.5)",borderRadius:5}}
+                           onPress={() => setModalVisible(false)}>
+                    <Ionicons name="chevron-back-outline" size={32} color="black"/>
+                    <Text style={{fontSize:16}}>All Recipes</Text>
+                </Pressable>
+                <ScrollView style={Styles.recipe_scrollView}>
+                    <Text style={Styles.recipe_name}>{recipe.name}</Text>
+                    <Text style={Styles.h2}>Ingredients</Text>
+                    {recipe.ingredients.map((e, i) => {
+                        return <Text key={i} style={Styles.recipe_ingredient}>
+                            {e.quantity} {e.name}
+                            {/*{e.type}*/}
+                        </Text>
+                    })}
+                    <Text style={Styles.h2}>Recipe</Text>
+                    {recipe.steps.map((e, i) => {
+                        return <Text key={i} style={Styles.recipe_step}>{e}</Text>
+                    })}
+                </ScrollView>
             </View>
         </Modal>
+
     </>
 }
--- Styles.js
+++ Styles.js
@@ -5,16 +5,19 @@ export const Styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        width:"100%"
+        width: "100%"
     },
     header: {
         fontSize: 24,
         fontWeight: "bold"
     },
-    recipe_steps: {
-        padding:5,
+    h2: {
+        fontSize: 18,
+        fontWeight: "bold",
+        paddingTop:5,
+        paddingBottom:5,
     },
-    mealCard: {
+    meal_card: {
         margin: 10,
         backgroundColor: "white",
         borderRadius: 20,
@@ -28,15 +31,42 @@ export const Styles = StyleSheet.create({
         shadowRadius: 4,
         elevation: 5,
     },
-    shoppingListCategory : {
-        fontSize:30,
-        borderColor:"grey",
-        borderTopWidth:1,
-        borderBottomWidth:1,
-        backgroundColor:"grey",
+    meal_card_image: {
+        width: 200,
+        height: 200,
+        borderRadius:5
     },
-    shoppingListItem : {
-        borderBottomWidth:1,
-        padding:5
+    shoppingListCategory: {
+        fontSize: 30,
+        borderColor: "grey",
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        backgroundColor: "grey",
+    },
+    shoppingListItem: {
+        borderBottomWidth: 1,
+        padding: 5
+    },
+    recipe_name: {
+        fontSize: 24,
+        fontWeight: "bold",
+        paddingBottom:20,
+    },
+    recipe_step: {
+        paddingTop:5,
+        paddingBottom:5,
+
+        color:"#484848",
+    },
+    recipe_ingredient: {
+        color:"grey",
+        paddingBottom:5,
+    },
+    recipe_scrollView: {
+        backgroundColor: "white",
+        flex: 1,
+        top: -20,
+        borderRadius: 10,
+        paddingHorizontal:15
     }
 });
